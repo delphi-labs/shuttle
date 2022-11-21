57a2d972b7351cf3c35d17122276c11a094dc0be
afsardo <afsardo@gmail.com>
2022-11-21 20:02:00 +0000

    feat: increase timeout for terra extension wallets

--- src/extensions/TerraExtension.ts
+++ src/extensions/TerraExtension.ts
@@ -84,7 +84,7 @@ export default class TerraExtension {
     return this.request("sign", { msgs: messages, prugeQueue: true, memo, fee });
   }
 
-  private request(type: string, data: any = {}, options: { timeout?: number } = { timeout: 15 }): Promise<any> {
+  private request(type: string, data: any = {}, options: { timeout?: number } = { timeout: 60 }): Promise<any> {
     return new Promise((resolve, reject) => {
       let promiseData: any = null;
       const onMessage = (event: any) => {
