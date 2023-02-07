8761f2da80730b017cdd072a13f6a51a5bc6827a
afsardo <afsardo@gmail.com>
2023-02-07 16:58:17 +0000

    ci: fix tests with async code

--- tests/react/context.test.tsx
+++ tests/react/context.test.tsx
@@ -3,23 +3,10 @@ import { render } from "@testing-library/react";
 
 import "jest-canvas-mock";
 
-import { MobileTerraStationProvider, ShuttleProvider, TerraStationProvider } from "../../src";
+import { ShuttleProvider } from "../../src";
 
 describe("ShuttleProvider render", () => {
   it("renders without crashing", () => {
-    render(
-      <ShuttleProvider
-        mobileProviders={[
-          new MobileTerraStationProvider({
-            networks: [],
-          }),
-        ]}
-        providers={[
-          new TerraStationProvider({
-            networks: [],
-          }),
-        ]}
-      />,
-    );
+    render(<ShuttleProvider mobileProviders={[]} providers={[]} />);
   });
 });
