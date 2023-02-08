78f4a27fa9d084592706e01ba835d623b3fe6fbf
afsardo <afsardo@gmail.com>
2023-02-08 22:21:29 +0000

    fix: zustand: stop importing create from the default export

--- src/react/store.ts
+++ src/react/store.ts
@@ -1,4 +1,4 @@
-import create, { StateCreator } from "zustand";
+import { create, StateCreator } from "zustand";
 import { WalletConnection } from "../internals/wallet";
 
 export type ShuttleStore = {
