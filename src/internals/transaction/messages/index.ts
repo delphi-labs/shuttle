76498b8501caa5d92c1079b9c7f046e82329e19d
wd021 <dannywitters@gmail.com>
2023-03-30 14:03:46 +0800

    add tests

--- src/internals/transaction/messages/index.ts
+++ src/internals/transaction/messages/index.ts
@@ -3,4 +3,5 @@ export * from "./MsgExecuteContract";
 export * from "./BasicAllowance";
 export * from "./MsgGrantAllowance";
 export * from "./MsgRevokeAllowance";
+export * from "./MsgSend";
 export * from "./MsgTransfer";
---
+++ tests/internals/transaction/messages/MsgSend.test.ts
@@ -0,0 +1,51 @@
+import { CosmosMsg, MsgSend } from "../../../../src";
+
+describe("MsgSend", () => {
+  test("it returns the correct typeUrl", () => {
+    const msg = new MsgSend({
+      from_address: "address1",
+      to_address: "address2",
+      amount: [{ denom: "uluna", amount: "100000" }],
+    });
+
+    expect(msg.typeUrl).toEqual("/cosmos.bank.v1beta1.MsgSend");
+  });
+
+  test("it converts to CosmosMsg", () => {
+    const msg = new MsgSend({
+      from_address: "address1",
+      to_address: "address2",
+      amount: [{ denom: "uluna", amount: "100000" }],
+    });
+
+    const cosmosMsg: CosmosMsg = msg.toCosmosMsg();
+
+    expect(cosmosMsg).toEqual({
+      typeUrl: "/cosmos.bank.v1beta1.MsgSend",
+      value: {
+        from_address: "address1",
+        to_address: "address2",
+        amount: [{ denom: "uluna", amount: "100000" }],
+      },
+    });
+  });
+
+  test("it converts to TerraExtension string", () => {
+    const msg = new MsgSend({
+      from_address: "address1",
+      to_address: "address2",
+      amount: [{ denom: "uluna", amount: "100000" }]
+    });
+
+    const terraExtensionMsg: string = msg.toTerraExtensionMsg();
+
+    expect(terraExtensionMsg).toEqual(
+      JSON.stringify({
+        "@type": "/cosmos.bank.v1beta1.MsgSend",
+        from_address: "address1",
+        to_address: "address2",
+        amount: [{ denom: "uluna", amount: "100000" }]
+      }),
+    );
+  });
+});
