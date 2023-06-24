4b74448c7c5621c7200a39f70a8c511ee615830f
afsardo <afsardo@gmail.com>
2023-06-24 19:45:04 +0100

    ci: fix tests

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { JestConfigWithTsJest } from "ts-jest";
-
-const jestConfig: JestConfigWithTsJest = {
-  preset: "ts-jest",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testEnvironment: "jsdom",
-};
-
-export default jestConfig;
