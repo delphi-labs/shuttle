f877489ca0ce82b8ce28cabc8a1a390214d8e1d0
afsardo <afsardo@gmail.com>
2023-07-29 04:48:11 +0100

    feat: fix issue with vite imports

--- examples/shuttle-port-vue/vite.config.ts
+++ examples/shuttle-port-vue/vite.config.ts
@@ -2,10 +2,12 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import vue from "@vitejs/plugin-vue";
+import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    viteCommonjs(),
     vue(),
     nodePolyfills({
       globals: {
@@ -20,6 +22,14 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ["@delphi-labs/shuttle", "@delphi-labs/shuttle-vue"],
+    esbuildOptions: {
+      plugins: [
+        // Solves:
+        // https://github.com/vitejs/vite/issues/5308
+        // add the name of your package
+        esbuildCommonjs(["cosmjs-types"]),
+      ],
+    },
   },
   build: {
     commonjsOptions: {
