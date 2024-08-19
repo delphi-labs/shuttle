18a73358672ffcce8d37c7a32710640ddbb5af92
afsardo <afsardo@gmail.com>
2024-08-19 13:31:14 +0100

    Update index.ts

--- packages/core/src/providers/mobile/index.ts
+++ packages/core/src/providers/mobile/index.ts
@@ -3,6 +3,5 @@ export * from "./CosmostationMobileProvider";
 export * from "./KeplrMobileProvider";
 export * from "./LeapCosmosMobileProvider";
 export * from "./MetamaskMobileProvider";
-export * from './OnseiMobileProvider';
+export * from "./OnseiMobileProvider";
 export * from "./WalletMobileProvider";
-
