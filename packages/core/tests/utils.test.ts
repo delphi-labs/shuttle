af3dae6d8438071f655b795bc34caaba3cd965d6
lotteam004 <lotteam004@proton.me>
2024-08-27 17:34:57 +0800

    add utils test case

--- packages/core/tests/utils.test.ts
+++ packages/core/tests/utils.test.ts
@@ -1,4 +1,4 @@
-import { defaultBech32Config } from "../src/utils";
+import { defaultBech32Config, objectToBase64, nonNullable } from "../src/utils";
 
 describe("defaultBech32Config", () => {
   test("returns the correct config", () => {
@@ -12,5 +12,46 @@ describe("defaultBech32Config", () => {
       bech32PrefixConsAddr: "marsvalcons",
       bech32PrefixConsPub: "marsvalconspub",
     });
+  });
+});
+
+describe("objectToBase64", () => {
+  test("should convert an object to a Base64 string", () => {
+    const obj = { name: "John", age: 30 };
+    const base64String = objectToBase64(obj);
+    expect(base64String).toBe(Buffer.from(JSON.stringify(obj)).toString("base64"));
+  });
+
+  test("should return an empty object as a Base64 string", () => {
+    const obj = {};
+    const base64String = objectToBase64(obj);
+    expect(base64String).toBe(Buffer.from(JSON.stringify(obj)).toString("base64"));
+  });
+
+  test("should handle nested objects", () => {
+    const obj = { user: { name: "John", age: 30 }, active: true };
+    const base64String = objectToBase64(obj);
+    expect(base64String).toBe(Buffer.from(JSON.stringify(obj)).toString("base64"));
+  });
+});
+
+describe("nonNullable", () => {
+  test("should return true for non-nullable values", () => {
+    expect(nonNullable("Hello")).toBe(true);
+    expect(nonNullable(42)).toBe(true);
+    expect(nonNullable({})).toBe(true);
+    expect(nonNullable([])).toBe(true);
+  });
+
+  test("should return false for null or undefined values", () => {
+    expect(nonNullable(null)).toBe(false);
+    expect(nonNullable(undefined)).toBe(false);
+  });
+
+  test("should work correctly wtesth type guards", () => {
+    const value: string | null = "Hello";
+    if (nonNullable(value)) {
+      expect(typeof value).toBe("string");
+    }
   });
 });
